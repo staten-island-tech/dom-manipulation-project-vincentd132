f6cb665bac0daaf6c5614579cd1a5f685521c388
vincentd132 <vincentd103@nycstudents.net>
2024-10-11 13:56:54 -0400

    w

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const DOMSelectors = {
   form: document.getElementById("songname"),
   form: document.getElementById("songimg"),
+  form: document.getElementById("button"),
 };
+
+document.getElementById("button").addEventListener("click", function () {
+  alert("Button was clicked!");
+});
