512c03d72d3beb9fc966eff57ef9f61f8d3a1232
vincentd132 <vincentd103@nycstudents.net>
2024-10-21 22:46:58 -0400

    w

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const DOMSelectors = {
-  form: document.getElementById("songname"),
-  form: document.getElementById("songimg"),
-  form: document.getElementById("button"),
+  form: document.getElementById("form"),
+  pop: document.getElementById("pop"),
 };
 
-document.getElementById("button").addEventListener("click", function () {
-  alert("Button was clicked!");
-});
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-}
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-document.getElementById("button").addEventListener("click", function () {
-  backgroundAndText(DOMSelectors, DOMSelectors.box);
+  const songname = document
+    .querySelector("input[name='songname']")
+    .value.trim();
+
+  DOMSelectors.pop.querySelector(".popup").innerHTML = "";
+
+  DOMSelectors.pop.querySelector(".popup").insertAdjacentHTML(
+    "beforeend",
+    `
+      <span class="close-btn">&times;</span> <!-- Close button -->
+      <h1 id="card-header">${songname}</h1>
+      <img id="card-image" src="${songimg}" alt="Song Image" onerror="this.onerror=null; this.src='path/to/default-image.jpg';" />
+    `
+  );
+
+  DOMSelectors.pop.style.display = "block";
+});
+
+DOMSelectors.pop.addEventListener("click", function (event) {
+  if (event.target.classList.contains("close-btn")) {
+    DOMSelectors.pop.style.display = "none";
+    location.reload();
+  }
 });
